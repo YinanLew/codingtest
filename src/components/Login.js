1d29226a31356b1a19240187a1b4019d80a73064
YinanLew <yinanlu0112@gmail.com>
2022-06-14 21:58:32 -0400

    coding test

---
+++ src/components/Login.js
@@ -0,0 +1,161 @@
+import React, {useRef, useState, useEffect, createContext} from 'react'
+import favicon from '../img/favicon.svg'
+import axios from 'axios'
+import Products from './Products'
+
+import EmailIcon from '@mui/icons-material/Email'
+import { IconButton, Link } from '@mui/material'
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined'
+import GitHubIcon from '@mui/icons-material/GitHub'
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{1,19}$/
+
+function Login() {
+  const userRef = useRef();
+
+
+
+  const [username, setUsername] = useState('');
+  const [date, setDate] = useState('');
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+  const [products, setProducts] = useState([]);
+  // const [productsPage, setProductsPage] = useState(true);
+
+  const [errMsg, setErrMsg] = useState('');
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, [])
+
+  useEffect(() => {
+    setValidName(USER_REGEX.test(username))
+    console.log(USER_REGEX.test(username))
+  }, [username])
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    const name = USER_REGEX.test(username)
+    if (!name) {
+      setErrMsg('Invalid Entry')
+    }
+    try {
+      const response = await axios.post(
+        'https://wemi-round2-server.herokuapp.com/round2/authentication',
+        {
+          displayName: username,
+          displayDate: date,
+        },
+      )
+      const token = response.data.access_token
+      window.localStorage.setItem('token', token)
+      const res = await axios.post(
+        'https://wemi-round2-server.herokuapp.com/round2/get-products',
+        {
+          page: 1,
+          amount: 50,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${window.localStorage.getItem('token')}`,
+          },
+        },
+      )
+      const products = res.data.products
+      console.log(res.data.products)
+      setProducts(products)
+    } catch (err) {
+      if (!err ? err.response : '') {
+        setErrMsg('No Server Response')
+        console.log(errMsg)
+      } else if (err.response ? err.response.status === 409 : '') {
+        setErrMsg('Username Taken')
+        console.log(errMsg)
+      } else {
+        setErrMsg('Failed')
+        console.log(errMsg)
+      }
+    }
+  }
+
+  return (
+    <>
+          <header className='header'>
+            <img src={favicon} alt='' />
+            <div className='header-p'>
+              <span>WELCOME TO WEMI ROUND 2</span>
+            </div>
+          </header>
+
+          <div className='login'>
+            <section>
+              <form onSubmit={handleSubmit}>
+                <div className='img-container'>
+                  <img src={favicon} alt='' />
+                </div>
+                <input
+                    type='text'
+                    id='username'
+                    ref={userRef}
+                    placeholder={'Username'}
+                    autoComplete='off'
+                    onChange={e => setUsername(e.target.value)}
+                    required
+                    aria-invalid={validName ? 'false' : 'true'}
+                    aria-describedby='uidnote'
+                    onFocus={() => setUserFocus(true)}
+                    onBlur={() => setUserFocus(false)}
+                />
+                <p
+                    id='uidnote'
+                    className={
+                      userFocus && username && !validName
+                          ? 'instructions'
+                          : 'offscreen'
+                    }
+                >
+                  Username must be 2-20 characters in length.
+                </p>
+
+                <input
+                    type='date'
+                    id='start'
+                    name='trip-start'
+                    value={date}
+                    min='2022-06-14'
+                    max='2025-12-31'
+                    onChange={e => setDate(e.target.value)}
+                />
+
+                <button className='token'>Get Token</button>
+              </form>
+            </section>
+          </div>
+          <footer className='footer'>
+            <div className='footer-icons'>
+              <IconButton>
+                <EmailIcon style={{ color: '#2CD889' }} />
+              </IconButton>
+              <span>example@gmail.com</span>
+            </div>
+            <div className='footer-icons'>
+              <IconButton>
+                <LocalPhoneOutlinedIcon style={{ color: '#2CD889' }} />
+              </IconButton>
+              <span>(647) 450-8627</span>
+            </div>
+            <div className='footer-icons'>
+              <IconButton>
+                <Link href='https://github.com/YinanLew/'>
+                  <GitHubIcon style={{ color: '#2CD889' }} />
+                </Link>
+              </IconButton>
+              <span>Github</span>
+            </div>
+          </footer>
+    </>
+)
+}
+
+export default Login
---
+++ src/components/Products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.css";
+
+function Products({products}) {
+    return (
+        <div>
+            {products}
+        </div>
+    );
+}
+
+export default Products;
